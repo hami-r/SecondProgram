fadb46499c7dc4f01c81eefc41df52cbef599ebb
hami-r <mynameissaitama54@gmail.com>
2022-11-21 14:38:43 +0530

    added odd or even and reverse number feature added

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        while (true) {
+        boolean isContinue = true;
+        while (isContinue) {
             System.out.println("""
                     <------------MENU--------------->
                     1. Largest
@@ -21,12 +22,14 @@ public class Main {
                     int numberTwo = scanner.nextInt();
                     int numberThree = scanner.nextInt();
                     System.out.println("Largest number = " + Math.max(Math.max(numberOne,numberTwo),numberThree));
+                    break;
                 case 2:
                     System.out.println("Enter three numbers: ");
                     numberOne = scanner.nextInt();
                     numberTwo = scanner.nextInt();
                     numberThree = scanner.nextInt();
-                    System.out.println("Smallest number = " + Math.min(Math.min(numberOne,numberOne),numberThree));
+                    System.out.println("Smallest number = " + Math.min(Math.min(numberOne,numberTwo),numberThree));
+                    break;
                 case 3:
                     System.out.println("Enter the number: ");
                     int number = scanner.nextInt();
@@ -42,7 +45,38 @@ public class Main {
                     } else {
                         System.out.println(number + " is a prime number");
                     }
+                    break;
+                case 4:
+                    System.out.println("Enter the number: ");
+                    number = scanner.nextInt();
+                    if(number % 2 == 0) {
+                        System.out.println(number + " is a even number");
+                    }
+                    else {
+                        System.out.println(number + " is a odd number");
+                    }
+                    break;
+                case 5:
+                    System.out.println("Enter the number: ");
+                    number = scanner.nextInt();
+                    StringBuilder rev = new StringBuilder(Integer.toString(number));
+                    System.out.println("Reverse with ending zeroes: " + rev.reverse());
+                    int storeNumber = number;
+                    int reverseNumber = 0;
+                    while(number > 0) {
+                        int digit = number % 10;
+                        reverseNumber = reverseNumber * 10 + digit;
+                        number /= 10;
+                    }
+                    System.out.println("Reverse of " + storeNumber + " is " + reverseNumber);
+                    break;
 
+                case 6:
+                    isContinue = false;
+                    break;
+                default:
+                    System.out.println("Enter a valid option!");
+                    break;
             }
         }
     }
